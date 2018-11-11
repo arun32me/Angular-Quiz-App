13f0adce299685ab7151781d564ebb61be9999ae
Arun Lalithambaran <40573656+arun32me@users.noreply.github.com>
2018-11-11 09:30:39 +0530

    theme ready

--- public/app/modules/login/directives/loginDirective.js
+++ public/app/modules/login/directives/loginDirective.js
@@ -2,8 +2,23 @@
 angular.module('quizApp')
 .directive('loginDirective', loginDirective);
 
+// loginDirective.$inject = [];
 function loginDirective() {
     return {
-        templateUrl: 'app/views/loginTemplate.html'
+        templateUrl: 'app/views/loginTemplate.html',
+        link: link
+    }
+    function link(scope, elemetnt, attr) {
+        function init() {
+            scope.user = {
+                email: 'arun',
+                password: '',
+                remember: false
+            }
+        }
+        init();
+        scope.auth = function() {
+
+        }
     }
 }
